93c478867a015f8572be163e5a37275d44f78fe8
MatthiasSaihttam <MatthiasSaihttam1@gmail.com>
2017-03-18 02:10:02 +0000

    Added username_available API endpoint.
    Add the following API endpoint /api/users/username_available?username=USERNAMEHERE. Returns '{"username_available":BOOLEAN}'

--- django_code/api/views.py
+++ django_code/api/views.py
@@ -1,10 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.contrib.auth.models import User
 
 # Create your views here.
 
-def users(request):
-    return error(request)
+def username_available(request):
+    username = request.GET.get("username", "")
+    if (username == ""):
+        return error(request)
+    try:
+        user = User.objects.get(username=username)
+        return HttpResponse('{"username_available":false}', content_type="application/json", status=200)
+    except User.DoesNotExist:
+        return HttpResponse('{"username_available":true}', content_type="application/json", status=200)
 
 def error(request):
     return HttpResponse('null', content_type="application/json", status=400)
