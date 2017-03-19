3a38aa2e797d3a2cb208c5e96581b637c591b810
MatthiasSaihttam <MatthiasSaihttam1@gmail.com>
2017-03-19 04:02:40 +0000

    Added Profile to extend off of the User class.
    So that users will also have a .profile attribute where we can put custom data outside of Django's defaults.
    Made and executed the appropriate migrations to store that data.

--- django_code/account/models.py
+++ django_code/account/models.py
@@ -1,5 +1,22 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 # Create your models here.
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    #max_length is not enforced automatically
+    bio = models.TextField(max_length=500, blank=True)
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.profile.save()
