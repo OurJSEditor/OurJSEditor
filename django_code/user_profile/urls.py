7858773928df2aa5fbccc819af42afab6aa577ed
MatthiasSaihttam <MatthiasSaihttam1@gmail.com>
2017-03-25 21:23:43 +0000

    Fixed a few bugs and started on profile edit screen

--- django_code/user_profile/urls.py
+++ django_code/user_profile/urls.py
@@ -5,4 +5,5 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^edit', views.edit, name='edit')
 ] 
--- django_code/user_profile/views.py
+++ django_code/user_profile/views.py
@@ -8,6 +8,16 @@ from django.contrib.auth.models import User
 def index(request, username):
     try:
         user = User.objects.select_related('profile').get(username=username)
-        return render(request, 'user_profile/index.html', {'user': user});
-    except User.doesNotExist:
+        return render(request, 'user_profile/index.html', {'user': user})
+    except User.DoesNotExist:
+        return render(request, 'user_profile/doesNotExist.html', {'username': username})
+
+def edit(request, username):
+    try:
+        user = User.objects.select_related('profile').get(username=username)
+        if (user.username == request.user.username):
+            return render(request, 'user_profile/editProfile.html')
+        else:
+            return render(request, 'user_profile/accessDenied.html')
+    except User.DoesNotExist:
         return render(request, 'user_profile/doesNotExist.html', {'username': username})
