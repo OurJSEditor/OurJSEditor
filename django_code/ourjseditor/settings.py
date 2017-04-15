506e54fee343fca5b787e8ad3853852dc4ae0089
MatthiasSaihttam <MatthiasSaihttam1@gmail.com>
2017-04-15 03:19:06 +0000

    Added static directory paths to settings file This is technically handled by Apache on the main server but for people running locally these are important.

--- django_code/ourjseditor/settings.py
+++ django_code/ourjseditor/settings.py
@@ -135,3 +135,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATIC_ROOT = ''
+
+STATICFILES_DIRS = ( os.path.join(os.path.dirname(BASE_DIR), "static/"), )
