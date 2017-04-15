bc46d1d8bc8fc047c568edb37cdcf219cdca8532
MatthiasSaihttam <MatthiasSaihttam1@gmail.com>
2017-04-15 03:23:14 +0000

    Updated allowed hosts Allowed both names for localhost and removed the link shortener thing because we have a real domain now.

--- django_code/ourjseditor/settings.py
+++ django_code/ourjseditor/settings.py
@@ -27,7 +27,7 @@ with open(os.path.join(BASE_DIR, "ourjseditor/secret_key.txt"), 'r') as f:
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['ourjseditor.com','45.56.88.22','ourjse.us.to']
+ALLOWED_HOSTS = ['ourjseditor.com','45.56.88.22','localhost','127.0.0.1']
 
 
 # Application definition
