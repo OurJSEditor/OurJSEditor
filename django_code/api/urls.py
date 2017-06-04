cd365152ad599d1d040e31205223a52b3492913b
Matthias <MatthiasSaihttam1@gmail.com>
2017-06-04 08:14:41 -0700

    Don't match /api/users
    Matching multiple different things for one API endpoint is needlessly
    confusing.

--- django_code/api/urls.py
+++ django_code/api/urls.py
@@ -3,6 +3,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^users?/username_available.*', views.username_available, name='username_available'),
+    url(r'^user/username_available.*', views.username_available, name='username_available'),
     url(r'^.*', views.error, name='error'),
 ]
