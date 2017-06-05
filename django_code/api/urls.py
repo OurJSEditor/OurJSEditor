0b72d8e0003a4e792dfd94e3391c03707e5be113
Matthias <MatthiasSaihttam1@gmail.com>
2017-06-05 14:04:26 -0700

    Program viewing API endpoint
    Added /api/program/PRG_ID API endpoint.

--- django_code/api/urls.py
+++ django_code/api/urls.py
@@ -5,5 +5,6 @@ from . import views
 urlpatterns = [
     url(r'^user/username_available', views.username_available, name='username_available'),
     url(r'^program/new', views.new_program, name="new-program-api"),
+    url(r'^program/([-_\w]{6})', views.view_program, name="view-program-api"),
     url(r'^.*', views.error, name='error'),
 ]
