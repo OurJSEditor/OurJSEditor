3fc3f4f2ceb2e120fbd5e613e7ba136f1f0c1fd9
Matthias <MatthiasSaihttam1@gmail.com>
2017-06-04 20:02:22 -0700

    New program API endpoint
    Added an API endpoint for creating programs. Still not 100% secure.
    Attempted to use more verbose error messages than previous commits.
    Removed unnecessary .* from username_available Regular Expression

--- django_code/api/urls.py
+++ django_code/api/urls.py
@@ -3,6 +3,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^user/username_available.*', views.username_available, name='username_available'),
+    url(r'^user/username_available', views.username_available, name='username_available'),
+    url(r'^program/new', views.new_program, name="new-program-api"),
     url(r'^.*', views.error, name='error'),
 ]
--- django_code/api/views.py
+++ django_code/api/views.py
@@ -2,6 +2,10 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.contrib.auth.models import User
 
+import json
+
+from program.models import Program
+
 # Create your views here.
 
 def username_available(request):
@@ -13,6 +17,30 @@ def username_available(request):
         return HttpResponse('{"username_available":false}', content_type="application/json", status=200)
     except User.DoesNotExist:
         return HttpResponse('{"username_available":true}', content_type="application/json", status=200)
+
+def new_program(request):
+    if request.method == 'POST':
+        try:
+            data = json.loads(request.body)
+            if request.user.is_authenticated:
+                program = Program.objects.create(
+                    user = request.user.profile,
+                    title = data["title"],
+                    html_content = data["html"],
+                    js_content = data["js"],
+                    css_content = data["css"],
+                )
+
+                response = HttpResponse('{"creationSuccess":true}', content_type="application/json", status=201)
+                response["Location"] = "/program/" + program.program_id
+                return response
+            else:
+                return HttpResponse('{"creationSuccess":false,"error":"Not logged in."}', content_type="application/json", status=403)
+        except ValueError:
+            # Catch and return 400 on malformed JSON
+            return HttpResponse('{"creationSuccess":false,"error":"Missing or malformed JSON."}', content_type="application/json", status=400)
+    else:
+        return HttpResponse("The method " + request.method + " is not allowed for the requested URL.", status=405)
 
 def error(request):
     return HttpResponse('null', content_type="application/json", status=400)
