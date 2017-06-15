8796362f558d39ef4b0a7129547e35a21019bd14
Matthias <MatthiasSaihttam1@gmail.com>
2017-06-14 21:21:41 -0700

    Make JSTinker code work with Django
    I won’t go everything this commit does, but basically when you request
    a program the server grabs the program’s data from storage, converts it
    to a JSON string which is inserted into the HTML page, where Javascript
    takes it and inserts it into the editor. Save button grabs data from
    the page, makes a JSON string, and sends it to the server.

--- django_code/program/urls.py
+++ django_code/program/urls.py
@@ -3,6 +3,5 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'new', views.new_program, name='new_program'),
-    url(r'^(.*)', views.program, name='program'),
+    url(r'(new|[-_\w]{6})', views.program, name='program'),
 ]
--- django_code/program/views.py
+++ django_code/program/views.py
@@ -3,11 +3,31 @@ from __future__ import unicode_literals
 
 from django.shortcuts import render
 from django.http import HttpResponse
-from django.utils import timezone
+from program.models import Program
+
+import json
 
 # Create your views here.
 def program (request, program_id):
-    return HttpResponse("")
+    data_dict = dict()
 
-def new_program (request):
-    return HttpResponse("")
+    if program_id == "new":
+        data_dict["new"] = True
+    else:
+        try:
+            current_program = Program.objects.select_related('user').get(program_id=program_id)
+
+            data_dict["id"] = program_id
+            data_dict["title"] = current_program.title
+            data_dict["js"] = current_program.js
+            data_dict["css"] = current_program.css
+            data_dict["html"] = current_program.html
+            data_dict["canEditProgram"] = (current_program.user == request.user)
+            data_dict["author"] = dict(
+                username=current_program.user.username,
+                displayName=current_program.user.profile.display_name
+            )
+        except Program.DoesNotExist:
+            return HttpResponse("404: No program found with that id", status=404)
+
+    return render(request, "program/index.html", {"data_dict": json.dumps(data_dict)})
