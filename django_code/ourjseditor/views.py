6f8fd30cdf87bd0e5644c0b096111f75e1f4f4bd
Matthias <MatthiasSaihttam1@gmail.com>
2017-09-20 18:04:58 -0700

    Add button to your profile from home page
    If you’re logged in you see a link to your profile from the home page
    of the webpage, instead of seeing nothing.

--- django_code/ourjseditor/views.py
+++ django_code/ourjseditor/views.py
@@ -3,4 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 
 def index(request):
-    return render(request, 'ourjseditor/index.html', { 'logged_in': request.user.is_authenticated })
+    return render(request, 'ourjseditor/index.html', {
+        'logged_in': request.user.is_authenticated,
+        'username': request.user.username
+    })
