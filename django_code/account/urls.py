43024b9d649050a7211796f2be9a22849c5b99aa
Matthias <MatthiasSaihttam1@gmail.com>
2019-02-05 11:50:03 -0800

    Fix password reset URLs
    Fixes #69. I mean, the issue was super vauge and makes it seem like this is a bigger problem, but I don't think it is. Hopefully everything works now.

--- django_code/account/urls.py
+++ django_code/account/urls.py
@@ -3,8 +3,8 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^forgot_password$',views.forgot_password, name='forgot-password'),
-    url(r'^reset_password$', views.reset_password, name='reset-password'),
+    url(r'^forgot-password$',views.forgot_password, name='forgot-password'),
+    url(r'^reset-password$', views.reset_password, name='reset-password'),
     url(r'^login$', views.login, name='login'), #Used for the UI for both creation and logging in
     url(r'^new$', views.new_user, name='new-user'), #Used by POST for creation
     url(r'^logout$', views.logout, name='logout'),
