9ae50c749f699a647ffe7420da6479b9828f6959
Matthias <MatthiasSaihttam1@gmail.com>
2019-05-12 21:34:07 -0700

    Add CSS & rename the JSX file
    -Moves the JSX associated with selecting a program template into "program-template-selector.jsx" from "new-program.jsx", to better reflect that this page isn't the new program page.
    -Adds CSS to style the boxes representing each option.

--- js/components/program-template-selector.jsx
+++ js/components/program-template-selector.jsx
@@ -19,20 +19,26 @@ export default class TemplateSelector extends Preact.Component {
     render () {
         const templateKeys = Object.keys(programTemplates);
 
-        return (
+        const content = (
             <div>
                 <h2>Select A Template</h2>
                 <div className="subheading">You're not picking a type of program you're bound to, just a template.</div>
-                {
-                    templateKeys.map(template => (
+                <div>
+                    {
+                        templateKeys.map(template => (
                         <TemplateType
                             description={programTemplates[template].description}
                             title={programTemplates[template].title}
                             src={template}
                             key={template} />
-                    ))
-                }
+                        ))
+                    }
+                </div>
             </div>
         );
+        
+        console.log(content);
+        
+        return content;
     }
 }
